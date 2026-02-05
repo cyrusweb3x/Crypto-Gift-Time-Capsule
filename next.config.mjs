23f1d98dc6ffdf0aa665518d7fe66c29641581f4
cyrusweb3x <cyrusweb3x@gmail.com>
2026-02-05 12:41:10 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
+    // TypeScript errors will be ignored during build
     ignoreBuildErrors: true,
   },
 };
